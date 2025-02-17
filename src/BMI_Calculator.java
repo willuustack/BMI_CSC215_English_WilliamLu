d7b4cc23e2d354cd4b5dfe156933220f6032bba1
will <william.loves.cake@gmail.com>
2025-02-16 20:37:18 -0800

    v1

--- src/BMI_Calculator.java
+++ src/BMI_Calculator.java
@@ -1,15 +1,83 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+public class BMI_Calculator {
+
+    public static void main(String[] args) {
+        intro(); //run intro
+
+        Scanner input = new Scanner(System.in);
+        boolean height_given = false;
+
+        System.out.print("Please enter your full name: ");
+        String name = input.nextLine();
+
+        int height_feet = 0;
+        int height_inches = 0;
+        boolean valid_height = false;
+
+        while (!valid_height) {
+            System.out.print("Please enter Height in feet and inches for " + name + ":");
+            try {
+                height_feet = input.nextInt();
+                height_inches = input.nextInt();
+                valid_height = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number");
+                input.next();
+            }
         }
+
+        int weight = 0;
+        boolean valid_weight = false;
+        while (!valid_weight) {
+            System.out.print("Please enter weight in pounds for " + name + ":");
+            try {
+                weight = input.nextInt();
+                valid_weight = true;
+            } catch (InputMismatchException e) {
+                System.out.println("Please enter a valid number");
+                input.next();
+            }
+        }
+
+
+        summary(name);//run summary
+        outro(name); //run outro
+
+        System.out.println("Your weight is: " + weight);
+        System.out.println("Your height is: " + height_feet + " " + height_inches);
+
+    }
+
+    public static void summary(String name) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy 'at' hh:mm:ss a");
+        String dateTime = now.format(formatter);
+        System.out.println("\n-- SUMMARY REPORT FOR " + name);
+        System.out.println("-- Date and Time: " + dateTime);
+
+
+    }
+
+
+    public static void intro() {
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("-- Welcome to:");
+        System.out.println("--            BODY MASS INDEX (BMI) Computation, CSC 215, English version");
+        System.out.println("--                                                                    by William Lu");
+        System.out.println("-----------------------------------------------------------------------------------------------");
+    }
+    public static void outro(String name) {
+        System.out.println("-----------------------------------------------------------------------------------------------");
+        System.out.println("-- Thank you for using my program, "+ name +"!");
+        System.out.println("-- Ear-esistible!!!");
+        System.out.println("-----------------------------------------------------------------------------------------------");
     }
 }
+
